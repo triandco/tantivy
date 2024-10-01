edfebfa9ee8fb2267b981cc89f22f0ac62f3cd71
Tri <tri@triandco.com>
2024-10-01 16:14:33 +1000

    make perfield postings writer pub from mod

--- src/postings/mod.rs
+++ src/postings/mod.rs
@@ -21,7 +21,7 @@ pub(crate) use stacker::compute_table_memory_size;
 
 pub use self::block_segment_postings::BlockSegmentPostings;
 pub(crate) use self::indexing_context::IndexingContext;
-pub(crate) use self::per_field_postings_writer::PerFieldPostingsWriter;
+pub use self::per_field_postings_writer::PerFieldPostingsWriter;
 pub use self::postings::Postings;
 pub(crate) use self::postings_writer::{serialize_postings, IndexingPosition, PostingsWriter};
 pub use self::segment_postings::SegmentPostings;
